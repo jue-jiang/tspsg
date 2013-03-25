fc4b742611941263ae73cf90635d9240c94dbd1c
Oleksii Serdiuk <contacts@oleksii.name>
2013-03-25 23:27:19 +0100

    Added proxy style and stylesheet to fix some UI issues on BlackBerry.

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,36 @@
 #   endif
 #endif
 
+#ifdef Q_OS_BLACKBERRY
+
+#include <QProxyStyle>
+
+class BB10ProxyStyle: public QProxyStyle
+{
+public:
+    int pixelMetric(PixelMetric metric, const QStyleOption *option, const QWidget *widget) const
+    {
+        switch (metric) {
+        case QStyle::PM_ButtonIconSize:
+            return 42;
+        case QStyle::PM_SmallIconSize:
+            return 42;
+        case QStyle::PM_LargeIconSize:
+            return 64;
+        default:
+        return QProxyStyle::pixelMetric(metric, option, widget);
+        }
+    }
+    int styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const
+    {
+        if (hint == QStyle::SH_UnderlineShortcut)
+            return 0;
+        return QProxyStyle::styleHint(hint, option, widget, returnData);
+    }
+};
+
+#endif // Q_OS_BLACKBERRY
+
 //#ifdef STATIC_BUILD
 //	#ifndef NOSVG
 //		Q_IMPORT_PLUGIN(qsvgicon)
@@ -54,6 +84,68 @@ QApplication app(argc, argv);
     app.setOrganizationDomain("oleksii.name");
     app.setApplicationName("TSP Solver and Generator");
     app.setApplicationVersion(BUILD_VERSION);
+
+#ifdef Q_OS_BLACKBERRY
+    QApplication::setStartDragDistance(42);
+    QApplication::setStyle(new BB10ProxyStyle);
+    app.setStyleSheet(
+                "QMenuBar {"
+                "    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #444 stop:0.65 #333, stop:1 #444);"
+                "    border-bottom: 1px solid darkgray;"
+                "    spacing: 1px;"
+                "}"
+                "QMenuBar::item {"
+                "    padding: 0.3em 0.5em;"
+                "}"
+                "QMenuBar::item:selected {"
+                "    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #00a8df stop:0.65 #00889f, stop:1 #00a8df);"
+                "}"
+                "QMenu {"
+                "    border: 1px solid darkgray;"
+                "}"
+                "QMenu::item {"
+                "    padding: 0.4em 0.5em 0.4em 1.4em;"
+                "    color: white;"
+                "    font-size: 9pt;"
+                "    background-color: #121212;"
+                "}"
+                "QMenu::item:selected {"
+                "    background-color: #00a8df;"
+                "}"
+                "QMenu::item:disabled {"
+                "    color: darkgray;"
+                "    background-color: #121212;"
+                "}"
+                "QMenu::icon {"
+                "    margin-left: 0.85em;"
+                "}"
+                "QTabBar {"
+                "    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #444 stop:0.65 #333, stop:1 #444);"
+                "    border-bottom: 1px solid darkgray;"
+                "    spacing: 1px;"
+                "}"
+                "QTabBar::tab {"
+                "    padding: 0.3em 0.5em;"
+                "}"
+                "QTabBar::tab:selected {"
+                "    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #00a8df stop:0.65 #00889f, stop:1 #00a8df);"
+                "    padding: 0.3em 0.5em;"
+                "}"
+                "QTableView {"
+                "    gridline-color: darkgray;"
+                "}"
+                "QTableView::item {"
+                "    padding: 0 0.5em;"
+                "}"
+                "QComboBox {"
+                "    padding: 0.5em 1.3em 0.5em 0.5em;"
+                "}"
+                "QTextBrowser {"
+                "    background-color: lightgray;"
+                "    color: black;"
+                "}"
+    );
+#endif // Q_OS_BLACKBERRY
 
     // Seeding random number generator
     qsrand(QDateTime::currentDateTime().toTime_t() ^ QCursor::pos().x() ^ QCursor::pos().y());
